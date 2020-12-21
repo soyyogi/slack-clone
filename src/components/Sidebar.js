fd8ec21a718813f194c78bf01cefd64dc70eef9b
yogi <raoyogesh78@yahoo.in>
2020-12-21 22:16:43 +0100

    #8 added sidebar options

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,15 @@ import React from 'react'
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import SidebarOption from './SidebarOption';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 function Sidebar() {
     return (
@@ -16,6 +25,15 @@ function Sidebar() {
                 </div>
                 <CreateIcon />
             </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
+            <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+            <SidebarOption Icon={DraftsIcon} title="Saved items"/>
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+            <SidebarOption Icon={AppsIcon} title="Apps"/>
+            <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+            <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+            <hr/>
             
         </div>
     )
