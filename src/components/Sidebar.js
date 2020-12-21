d569bf03a5c7a2fb80f8c662dbb2cb37175aec15
yogi <raoyogesh78@yahoo.in>
2020-12-21 22:41:07 +0100

    #4 add channel icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,6 +11,8 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -34,7 +36,9 @@ function Sidebar() {
             <SidebarOption Icon={FileCopyIcon} title="File browser"/>
             <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
             <hr/>
-            
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+            <hr/>
+            <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
         </div>
     )
 }
