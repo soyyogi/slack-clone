e0b1496b3c7c5289b1a271ec1eb0cb9de57a1df7
yogi <raoyogesh78@yahoo.in>
2020-12-21 21:03:29 +0100

    #3 added sidebar header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       {/* <LandingPage /> */}
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   )
 }
