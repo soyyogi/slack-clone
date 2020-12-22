0374c4a96d8759be796dd4cdd18a518af78c1486
yogi <raoyogesh78@yahoo.in>
2020-12-22 15:42:15 +0100

    #7 added chat screen header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -17,9 +18,16 @@ function App() {
             <Header />
             <div className="app__body">
               <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/room">
+                  <h3>Welcome</h3>
+                </Route>
+              </Switch>
             </div>
           </Route>
-          
         </Switch>
         
       </Router>
---
+++ src/components/Chat.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Chat.css';
+import { useParams } from 'react-router-dom';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+
+function Chat() {
+
+    const { roomId } = useParams();
+
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong># general</strong>
+                        <StarBorderOutlinedIcon />
+                    </h4>
+                </div>
+                <div className="chat__headerRight">
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
