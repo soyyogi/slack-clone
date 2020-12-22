749e3f6aaa0b28e9f09a1f0a41a2b880eb2537af
yogi <raoyogesh78@yahoo.in>
2020-12-22 10:04:19 +0100

    #4 set channels from db and render all

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
@@ -13,8 +13,24 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
+import db from '../firebase';
 
 function Sidebar() {
+
+    const [channels, setChannels] = useState([]);
+
+    //set channels from db once the sidebar is rendered
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot => (
+            setChannels(
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    name: doc.data().name
+                }))
+            )
+        ))
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -39,6 +55,11 @@ function Sidebar() {
             <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
             <hr/>
             <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
+
+            {/* render the collection of channels */}
+            {channels.map(channel => (
+                <SidebarOption title={channel.name} id={channel.id} />
+            ))}
         </div>
     )
 }
