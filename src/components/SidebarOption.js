87abc1fa931f0799acf6993deaaf31802ccb740d
yogi <raoyogesh78@yahoo.in>
2020-12-22 16:00:41 +0100

    #4 added select channel feature

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './SidebarOption.css';
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
+
+    const history = useHistory();
+
+    const selectChannel = () => {
+        if (id) {
+            history.push(`/room/${id}`)
+        } else {
+            history.push(title)
+        }
+    }
+
+    const addChannel = () => {}
+
     return (
-        <div className="sidebarOption">
+        <div className="sidebarOption" onClick={addChannelOption ? addChannel : selectChannel}>
             {Icon && <Icon className="sidebarOption__icon" />}
             {Icon ? <h3>{title}</h3> : <h3 className="sidebarOption__channel"><span className="sidebarOption__hash">#</span> {title}</h3>}
         </div>
