6a6ed4805336757d382068c46d73a41d227f7de4
yogi <raoyogesh78@yahoo.in>
2020-12-22 16:26:05 +0100

    # updated chat room name in chat room header

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../firebase';
 
 function Chat() {
 
     const { roomId } = useParams();
+
+    const [ roomDetails, setRoomDetails] = useState(null);
+
+    //set room details whenever roomid changes
+    useEffect(() => {
+        if (roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
+                setRoomDetails(snapshot.data())
+            ))
+        }
+    }, [roomId])
 
     return (
         <div className="chat">
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
-                        <strong># general</strong>
+                        <strong># {roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon />
                     </h4>
                 </div>
