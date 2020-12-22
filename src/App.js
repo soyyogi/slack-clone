e25b2a87e3ac99eb75fd563e18a61a0cc53775bc
yogi <raoyogesh78@yahoo.in>
2020-12-22 10:48:52 +0100

    #6 added react router and linked landing page with chat room

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css'
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      {/* <LandingPage /> */}
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/room">
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+            </div>
+          </Route>
+          
+        </Switch>
+        
+      </Router>
     </div>
   )
 }
