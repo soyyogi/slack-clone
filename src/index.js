a1201cc33a677506d21851b989579519701eb392
yogi <raoyogesh78@yahoo.in>
2020-12-23 13:59:45 +0100

    #10 added state provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './components/StateProvider';
+import Reducer, { initialState } from './components/Reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={Reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
