32dae0b8e9b0ff03729e12ebedd25996bb1a5712
yogi <raoyogesh78@yahoo.in>
2020-12-23 15:05:08 +0100

    #11 added chat input component and realtime rendering

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,6 +5,7 @@ import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import db from '../firebase';
 import Message from './Message';
+import ChatInput from './ChatInput';
 
 function Chat() {
 
@@ -55,6 +56,8 @@ function Chat() {
                     />
                 ))}
             </div>
+
+            <ChatInput channelName={roomDetails?.name} channelId={roomId} />
         </div>
     )
 }
---
+++ src/components/ChatInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import db from '../firebase';
+import './ChatInput.css'
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
+
+function ChatInput({ channelName, channelId }) {
+    const [ input, setInput ] = useState('');
+    const [{ user }] = useStateValue();
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if (channelId) {
+            db.collection('rooms').doc(channelId).collection('messages').add({
+                message: input,
+                timestamp:  firebase.firestore.FieldValue.serverTimestamp(),
+                user: user.displayName,
+                userimage: user.photoURL,
+            })
+        }
+
+        setInput('');
+    }
+
+    return (
+        <div className="chatInput">
+            <form>
+                <input 
+                value={input}
+                onChange={e => setInput(e.target.value)}    
+                placeholder={`Type your message #${channelName}`} type="text"/>
+                <button type="submit" onClick={sendMessage}>Send</button>
+            </form>
+        </div>
+    )
+}
+
+export default ChatInput
